da01085f0151e5054e4f9ab773575ef28bc9afef
Gabriele Lisci <gabriele.lisci@rutgers.edu>
2025-10-24 16:33:45 -0400

    Testing my first commit before starting my work. Yippie!

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -14,7 +14,7 @@
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
-  */
+  *Gabe/
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
